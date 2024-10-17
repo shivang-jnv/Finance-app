d94dee961f484a4975a745ecf83183fb0c8c0100
Shivang Kanaujia <skjnvspn@gmail.com>
2024-10-17 13:56:51 +0530

    designed navbar and dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,18 @@
+import { ClerkLoaded, ClerkLoading, SignOutButton, UserButton, } from "@clerk/nextjs"
+import { Loader2 } from "lucide-react"
 
 export default function Home() {
  return (
-  <p>
-    this is an authenticated route
-  </p>
+  <div className="mt-2">
+          <ClerkLoaded>
+            <SignOutButton>
+              <UserButton />
+            </SignOutButton>
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+  
  )
 }
